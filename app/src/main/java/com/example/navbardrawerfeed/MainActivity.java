8f4fa72ddd86ec2fd404a17ec73ee41c77686353
Prayanshu <prayanshubit@gmail.com>
2020-10-27 17:44:20 +0530

    issue: fragment changes to home automatically on orientation change, no problem with CNB on orientation change

---
+++ app/src/main/java/com/example/navbardrawerfeed/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.navbardrawerfeed;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    //keep minSdk 23 or above(for chip nav bar to work properly)
+
+    private ChipNavigationBar chipNavigationBar;
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chipNavigationBar = findViewById(R.id.bottom_nav_bar);
+
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigation_drawer);
+        drawer = findViewById(R.id.drawer);
+
+        setSupportActionBar(toolbar);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        chipNavigationBar.setItemSelected(R.id.home, true);
+
+        if(chipNavigationBar.getSelectedItemId()==R.id.home)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        else if(chipNavigationBar.getSelectedItemId()==R.id.event)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EventsFragment()).commit();
+        else
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MeFragment()).commit();
+
+
+        BottomMenu(); //for handling clicks on chipNavigationBar
+
+    }
+
+
+
+    private void BottomMenu() {
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i){
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.event:
+                        fragment = new EventsFragment();
+                        break;
+                    case R.id.me:
+                        fragment = new MeFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
+
+    }
+
+}
